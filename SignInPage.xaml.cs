7568980eb787f3fac75b939370543f350e3adcab
ahmedaymanfarid <ahmedaymanfarid@outlook.com>
2021-04-25 12:21:34 +0200

    Added Sign-In and Sign-Up Pages

---
+++ SignInPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+namespace _01electronics_crm
+{
+    /// <summary>
+    /// Interaction logic for SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : Page
+    {
+        IntegrityChecks integrityChecker = new IntegrityChecks();
+
+        String employeeEmail;
+        String employeePassword;
+
+        Employee loggedInUser;
+
+        public SignInPage()
+        {
+            InitializeComponent();
+
+            loggedInUser = new Employee();
+        }
+
+        private void OnButtonClickedSignIn(object sender, RoutedEventArgs e)
+        {
+            employeeEmail = employeeEmailTextBox.Text;
+
+            if (!integrityChecker.CheckEmployeeLoginEmailEditBox(employeeEmail, ref employeeEmail))
+                return;
+
+            loggedInUser.InitializeEmployeeInfo(employeeEmail);
+
+            employeePassword = employeePasswordTextBox.Text;
+
+            if (!integrityChecker.CheckEmployeePasswordEditBox(employeePassword, loggedInUser.GetEmployeeId()))
+                return;
+
+            PortalWindow portalWindowOpen = new PortalWindow(ref loggedInUser);
+            portalWindowOpen.Show();
+        }
+
+        private void OnButtonClickedSignUp(object sender, RoutedEventArgs e)
+        {
+            SignUpPage signUp = new SignUpPage();
+            this.NavigationService.Navigate(signUp);
+        }
+    }
+}
---
+++ UserPortalPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01electronics_crm
+{
+    /// <summary>
+    /// Interaction logic for UserPortalPage.xaml
+    /// </summary>
+    /// 
+    public partial class UserPortalPage : Page
+    {
+
+        private Employee loggedInUser;
+
+        public UserPortalPage(ref Employee mLoggedInUser)
+        {
+            InitializeComponent();
+
+            loggedInUser = mLoggedInUser;
+
+            InitializeEmployeeDashboard();
+        }
+        
+        private void InitializeEmployeeDashboard()
+        {
+            employeeNameLabel.Content = loggedInUser.GetEmployeeName();
+            employeeBirthdateLabel.Content = loggedInUser.GetEmployeeBirthDate();
+            employeeJoiningDateLabel.Content = loggedInUser.GetEmployeeJoinDate();
+            employeeDepartmentLabel.Content = loggedInUser.GetEmployeeDepartment();
+            employeeTeamLabel.Content = loggedInUser.GetEmployeeTeam();
+            employeeBusinessEmailLabel.Content = loggedInUser.GetEmployeeBusinessEmail();
+            employeePersonalEmailLabel.Content = loggedInUser.GetEmployeePersonalEmail();
+        }
+
+        private void OnButtonClickedMyProfile(object sender, RoutedEventArgs e)
+        {
+            UserPortalPage userPortal = new UserPortalPage(ref loggedInUser);
+            this.NavigationService.Navigate(userPortal);
+        }
+
+        private void OnButtonClickedEmployees(object sender, RoutedEventArgs e)
+        {
+            EmployeesPage employees = new EmployeesPage(ref loggedInUser);
+            this.NavigationService.Navigate(employees);
+        }
+
+        private void OnButtonClickedPayrollInfo(object sender, RoutedEventArgs e)
+        {
+            PayrollInfoPage payrollInfo = new PayrollInfoPage(ref loggedInUser);
+            this.NavigationService.Navigate(payrollInfo);
+        }
+
+        private void OnButtonClickedSalaries(object sender, RoutedEventArgs e)
+        {
+            SalariesPage salaries = new SalariesPage(ref loggedInUser);
+            this.NavigationService.Navigate(salaries);
+        }
+
+        private void OnButtonClickedAbsence(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnButtonClickedVacations(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnEmployeeInfoDoubleClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
