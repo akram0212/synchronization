43bb01f00132c29806bfc9502b6eaf31f58e965d
Salma <salma.omran@01electronics.net>
2021-04-28 12:32:00 +0200

    Solved Employee.cs bug

--- Employee.cs
+++ Employee.cs
@@ -67,16 +67,10 @@ namespace _01electronics_erp
 		employees_info.employee_department, 
 		employees_info.employee_team, 
 		employees_info.employee_position, 
-		employees_info.graduation_year, 
 
-		educational_degrees.id, 
-		educational_majors.id, 
-
-        banks_names.id,
-        employees_payroll_info.payroll_id,
-        employees_payroll_info.account_id,
-
-        employees_salaries.salary,
+		employees_educational_qualifications.graduation_year, 
+		employees_educational_qualifications.certificate, 
+		employees_educational_qualifications.major,
 
 		employees_info.birth_date, 
 		employees_info.join_date, 
@@ -94,9 +88,7 @@ namespace _01electronics_erp
 		educational_degrees.educational_degree, 
 		educational_majors.educational_major, 
 
-        employees_initials.employee_initial,
-
-		banks_names.bank_name
+        employees_initials.employee_initial
 
 		from erp_system.dbo.employees_info 
 		inner join erp_system.dbo.departments_type 
@@ -116,9 +108,7 @@ namespace _01electronics_erp
 		left join erp_system.dbo.employees_initials 
 		on employees_info.employee_id = employees_initials.id 
 		left join erp_system.dbo.educational_degrees 
-		on employees_info.educational_degree = educational_degrees.id 
 		left join erp_system.dbo.educational_majors 
-		on employees_info.major = educational_majors.id 
         left join erp_system.dbo.employees_salaries
         on employees_info.employee_id = employees_salaries.id
         left join erp_system.dbo.employees_payroll_info
@@ -135,11 +125,11 @@ namespace _01electronics_erp
 
             BASIC_STRUCTS.SQL_COLUMN_COUNT_STRUCT queryColumns = new BASIC_STRUCTS.SQL_COLUMN_COUNT_STRUCT();
 
-            queryColumns.sql_int = 9;
-            queryColumns.sql_bigint = 1;
-            queryColumns.sql_money = 1;
+            queryColumns.sql_int = 7;
+            queryColumns.sql_bigint = 0;
+            queryColumns.sql_money = 0;
             queryColumns.sql_datetime = 2;
-            queryColumns.sql_string = 12;
+            queryColumns.sql_string = 11;
 
             if (!initializationObject.GetRows(sqlQuery, queryColumns, BASIC_MACROS.SEVERITY_HIGH))
                 return false;
@@ -152,8 +142,6 @@ namespace _01electronics_erp
 
             educationalQualificationId = initializationObject.rows[0].sql_int[5];
             majorId = initializationObject.rows[0].sql_int[6];
-
-            salary = initializationObject.rows[0].sql_money[0];
 
             birthDateStruct = initializationObject.rows[0].sql_datetime[0];
             joinDateStruct = initializationObject.rows[0].sql_datetime[1];
@@ -176,21 +164,6 @@ namespace _01electronics_erp
             major = initializationObject.rows[0].sql_string[9];
 
             initials = initializationObject.rows[0].sql_string[10];
-
-            payrollInfo = new List<COMPANY_ORGANISATION_MACROS.BANK_STRUCT>();
-
-            for (int i = 0; i < initializationObject.rows.Count; i++)
-            {
-                COMPANY_ORGANISATION_MACROS.BANK_STRUCT tempItem = new COMPANY_ORGANISATION_MACROS.BANK_STRUCT();
-
-                tempItem.bank_id = initializationObject.rows[i].sql_int[7];
-                tempItem.payroll_id = initializationObject.rows[i].sql_int[8];
-                tempItem.account_id = (ulong)initializationObject.rows[i].sql_bigint[0];
-
-                tempItem.bank_name = initializationObject.rows[i].sql_string[11];
-                
-                payrollInfo.Add(tempItem);
-            }
 
             return true;
         }
