0bd724019dea522001dc2e7493937a523cfc96dc
ahmedaymanfarid <ahmedaymanfarid@outlook.com>
2021-06-14 13:59:56 +0200

    Adding Add company and contact windows

---
+++ AddContactWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using _01electronics_erp;
+
+namespace _01electronics_crm
+{
+    /// <summary>
+    /// Interaction logic for AddContactWindow.xaml
+    /// </summary>
+    public partial class AddContactWindow : Window
+    {
+        protected String sqlQuery;
+
+        public Employee loggedInUser;
+        public Contact contact;
+
+        protected SQLServer sqlServer;
+        protected CommonQueries commonQueries;
+        protected CommonFunctions commonFunctions;
+        protected IntegrityChecks integrityChecker;
+
+        public AddContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        public AddContactWindow(ref Employee mLoggedInUser)
+        {
+            loggedInUser = mLoggedInUser;
+
+            sqlServer = new SQLServer();
+            commonQueries = new CommonQueries();
+            commonFunctions = new CommonFunctions();
+            integrityChecker = new IntegrityChecks();
+
+            contact = new Contact();
+
+            InitializeComponent();
+
+        }
+    }
+}
