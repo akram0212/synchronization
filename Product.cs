ce580bffc7008e51cb33fce2da5b84b6ca35fea3
ahmedaymanfarid <ahmedaymanfarid@outlook.com>
2021-08-25 10:19:03 +0200

    Deleted unwanted changes

--- Product.cs
+++ Product.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using _01electronics_hr;
 using _01electronics_library;
 
 namespace _01electronics_crm
