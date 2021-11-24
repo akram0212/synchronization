559d746ad1799d84cd8b935daf68c0be240506af
ahmedsameh01 <ahmed.sameh@01electronics.net>
2021-11-24 14:18:28 +0200

    removed comment on signin page password for release

--- SignInPage.xaml.cs
+++ SignInPage.xaml.cs
@@ -47,8 +47,8 @@ namespace _01electronics_crm
 
             employeePassword = employeePasswordTextBox.Password;
 
-            //if (!integrityChecker.CheckEmployeePasswordEditBox(employeePassword, loggedInUser.GetEmployeeId()))
-            //    return;
+            if (!integrityChecker.CheckEmployeePasswordEditBox(employeePassword, loggedInUser.GetEmployeeId()))
+                return;
 
             MainWindow mainWindowOpen = new MainWindow(ref loggedInUser);
 
