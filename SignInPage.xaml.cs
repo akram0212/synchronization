3ab570a1f6e03cee95d79b96e892d07011b2c9cf
ahmedaymanfarid <ahmedaymanfarid@outlook.com>
2022-03-02 13:11:55 +0200

    Solved conflicts

--- SignInPage.xaml.cs
+++ SignInPage.xaml.cs
@@ -47,13 +47,9 @@ namespace _01electronics_crm
 
             employeePassword = employeePasswordTextBox.Password;
 
-<<<<<<< HEAD
-           // if (!integrityChecker.CheckEmployeePasswordEditBox(employeePassword, loggedInUser.GetEmployeeId()))
-             //   return;
-=======
-            //if (!integrityChecker.CheckEmployeePasswordEditBox(employeePassword, loggedInUser.GetEmployeeId()))
-              // return;
->>>>>>> dbf586f5f8765193e7b6ef6579949af296cdbbd0
+
+           if (!integrityChecker.CheckEmployeePasswordEditBox(employeePassword, loggedInUser.GetEmployeeId()))
+                return;
 
             MainWindow mainWindowOpen = new MainWindow(ref loggedInUser);
 
