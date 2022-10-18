bdd569cc07a3750692ed587932d3f1a60e9bac7d
Akram Mohamed <akram.mohamed@01electronics.local>
2022-10-18 13:54:58 +0200

    background thread updated

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,7 @@ namespace _01electronics_crm
     /// </summary>
     public partial class MainWindow : NavigationWindow
     {
+        int progress = 0;
         bool closedWindow = false;
         private BackgroundWorker backgroundWorker = new BackgroundWorker();
         //Timer timer=new Timer(60000);
@@ -27,7 +28,6 @@ namespace _01electronics_crm
 
             this.Closing += NavigationWindow_Closing;
 
-  
             InitializeComponent();
             if (!File.Exists(Directory.GetCurrentDirectory() + "\\Track.txt"))
             {
@@ -42,6 +42,7 @@ namespace _01electronics_crm
             if (fileFound == false)
             {
                 backgroundWorker.DoWork += BackgroundStart;
+                backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
                 backgroundWorker.RunWorkerAsync();
             }
 
@@ -53,7 +54,12 @@ namespace _01electronics_crm
 
         }
 
- 
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progress = e.ProgressPercentage;
+        }
+
+
         //private void OnTimerElapsed(object o, ElapsedEventArgs s)
         //{
         //    if (ftpServer.CheckDateChanged() == false)
@@ -70,6 +76,8 @@ namespace _01electronics_crm
 
         private void BackgroundStart(object sender,DoWorkEventArgs e)
         {
+            backgroundWorker.WorkerReportsProgress = true;
+
             String errorMessage = String.Empty;
             if (!ftpServer.DownloadFolder(BASIC_MACROS.MODELS_PHOTOS_PATH,Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\01 Electronics\\erp_system\\products_photos\\", ref errorMessage))
             {
@@ -78,6 +86,7 @@ namespace _01electronics_crm
 
 
             backgroundWorker.ReportProgress(100);
+
             if (closedWindow == true)
             {
                 CancelEventArgs cancelEventArgs = new CancelEventArgs();
@@ -85,6 +94,8 @@ namespace _01electronics_crm
                 NavigationWindow_Closing(null, cancelEventArgs);
             }
 
+
+            Thread.CurrentThread.Suspend();
 
         }
 
@@ -109,13 +120,31 @@ namespace _01electronics_crm
         private void NavigationWindow_Closing(object sender, CancelEventArgs e)
         {
             closedWindow = true;
-            if (backgroundWorker.WorkerReportsProgress == true)
-                this.Close();
-            else
+
+            this.Dispatcher.Invoke(() =>
             {
-                e.Cancel = true;
-                this.Hide();
-           }
+
+                if (backgroundWorker.IsBusy == true)
+                {
+
+                    if (progress == 100)
+                    {
+
+                        e.Cancel = false;
+                    }
+                    else
+                    {
+                        e.Cancel = true;
+                        this.Hide();
+                    }
+
+                }
+                else
+                    e.Cancel = false;
+               
+
+            });
+           
         }
 
         private void NavigationWindow_Closed(object sender, EventArgs e)
