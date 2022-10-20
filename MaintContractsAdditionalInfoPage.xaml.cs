bfeb0160fa0d9f0bc8bc254448749cbe34e8b71d
Salma Omran <salma.omran@01electronics.local>
2022-10-20 16:26:44 +0200

    commit to pull akram's work

--- MaintContractsAdditionalInfoPage.xaml.cs
+++ MaintContractsAdditionalInfoPage.xaml.cs
@@ -598,7 +598,7 @@ namespace _01electronics_crm
 
             if (quantity != 0)
             {
-                if (viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_EDIT_CONDITION || viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_VIEW_CONDITION)
+                if (viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_VIEW_CONDITION)
                 {
                     ////// INITIALIZE FIRST GRID VALUES ///////
 
@@ -614,7 +614,7 @@ namespace _01electronics_crm
                         firstRowGrid.Visibility = Visibility.Collapsed;
 
                 }
-                else if (viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_RENEW_CONDITION)
+                else if (viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_EDIT_CONDITION || viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_RENEW_CONDITION)
                 {
                     ////// INITIALIZE FIRST GRID VALUES ///////
 
@@ -661,7 +661,7 @@ namespace _01electronics_crm
                     gridI.Children.Add(serialTextBox);
                     modelSerialGrid.Children.Add(gridI);
 
-                    if(viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_EDIT_CONDITION || viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_VIEW_CONDITION)
+                    if(viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_VIEW_CONDITION)
                     {
                         serialTextBox.IsEnabled = false;
 
@@ -671,7 +671,7 @@ namespace _01electronics_crm
                         serialTextBox.Text = currentModelSerial.model_serial;
 
                     }
-                    else if(viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_RENEW_CONDITION)
+                    else if(viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_EDIT_CONDITION || viewAddCondition == COMPANY_WORK_MACROS.CONTRACT_RENEW_CONDITION)
                     {
                         BASIC_STRUCTS.MODEL_SERIAL_STRUCT currentModelSerial = maintenanceContract.GetMaintContractModelsSerialsList().Find
                             (tmpSerial => tmpSerial.product_id == index + 1 && tmpSerial.model_serial_id == i + 1);
